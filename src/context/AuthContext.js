0b926b498e47d003d4a4166e360375e61b75060b
xoxo <thakursatyam48@gmail.com>
2022-06-04 01:43:25 +0530

    redirect not popup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,7 +2,7 @@
 import { useContext, createContext, useEffect, useState } from "react";
 import {
   GoogleAuthProvider,
-  signInWithPopup,
+  signInWithRedirect,
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
@@ -15,7 +15,7 @@ export const AuthContextProvider = ({ children }) => {
 
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider);
+    signInWithRedirect(auth, provider);
   };
 
   const logOut = () => {
